19c772bb3807c24e6e49703b2252bb4ffa3ef80c
Jackblow33 <jackblow33@hotmail.com>
2025-03-26 19:26:08 -0400

    Update BaseDebian.sh

--- BaseDebian.sh
+++ BaseDebian.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-##########   Working as of March 26 2025 - Select Debian with no dektop environnement ex. KDE, gnome, etc...
+##########   Working to install packages
+# March 26 2025 - Select Debian with no dektop environnement ex. KDE, gnome, etc...
 
 #Link Debian-testing weekly build iso amd64
 #https://cdimage.debian.org/cdimage/weekly-builds/amd64/iso-cd/debian-testing-amd64-netinst.iso
