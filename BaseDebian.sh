cd0a3bc9b9bacac220923bd75b2741b4bcff22cc
Jackblow33 <jackblow33@hotmail.com>
2025-03-26 01:32:14 -0400

    Update BaseDebian.sh

--- BaseDebian.sh
+++ BaseDebian.sh
@@ -3,6 +3,7 @@
 #Link Debian-testing weekly build iso amd64
 #https://cdimage.debian.org/cdimage/weekly-builds/amd64/iso-cd/debian-testing-amd64-netinst.iso
 
+#echo $PATH
 #cd /usr/local/bin
 #git clone https://github.com/Jackblow33/DebianMultiScripts.git
 #cd DebianMultiScripts
