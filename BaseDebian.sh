5f30b09ec0f920b29827bf8d281189742d192e7f
Jackblow33 <jackblow33@hotmail.com>
2025-03-27 21:15:39 -0400

    Update BaseDebian.sh

--- BaseDebian.sh
+++ BaseDebian.sh
@@ -44,28 +44,35 @@ echo
 echo "Congratulation!"
 echo
 
-read -p "Press enter to reboot............................>>>"
-reboot
+#read -p "Install completed! Press Enter to boot............................>>>"
 
+#Enable Network manager
+     sudo systemctl enable NetworkManager
+     sudo systemctl start NetworkManager
+#Enable sddm so it get started on boot
+    sudo systemctl enable sddm
+#Boot    
+    sudo systemctl start sddm
+
+
+    
+    ### Jackblow33 2025 #####################
+
+
+
+    
     
    
 ### TODO ###
-#1-Add Bluetooth applet - find related pkg
-    #bluetooth bluez-meshd bluez-cups     # bluedevil 
-#2-Cleanup menu and add menu editor config file + remove kmenuedit from install script
-#3-Automated option to remove or not screen brightnest applet | laptop vs desktop with command hostnamectl
-#4-deactivate top left screen edge /overview set to no action  #https://github.com/shalva97/kde-configuration-files?tab=readme-ov-file#screen-edges
-#5-add commands from postinstall document
-#6-add error checking, press enter, reboot
-#7-unpin konquerer from task bar | pin firefox Konsole   #https://github.com/shalva97/kde-configuration-files
-#9-remove konsole exit confirmation
-#10-set google as default home page in firefox
-#100-install without emoji, konquerer, kedit - for now hidden from menu but still present...
+# plasma-nm setup
+# Cleanup menu and add menu editor config file + remove kmenuedit from install script + up config file to github
+# Automated option to remove or not screen brightnest applet | laptop vs desktop with command hostnamectl
+# deactivate top left screen edge /overview set to no action  #https://github.com/shalva97/kde-configuration-files?tab=readme-ov-file#screen-edges
+# add commands from postinstall document
+# add error checking, press enter, reboot
+# unpin konquerer from task bar | pin firefox Konsole   #https://github.com/shalva97/kde-configuration-files
+# remove konsole exit confirmation
+# set google as default home page in firefox
+# install without emoji, konquerer, kedit - for now hidden from menu but still present...
 
-#Delete
-    #Enable Network manager
-    #sudo systemctl enable NetworkManager
-    #sudo systemctl start NetworkManager
-    #Enable sddm so it get started on boot
-    #sudo systemctl enable sddm
-    #sudo systemctl start sddm
+
