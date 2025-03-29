90facceca0730a85db4f1a5958eeed9ca76247d7
Jackblow33 <jackblow33@hotmail.com>
2025-03-29 02:47:48 -0400

    Update BaseDebian.sh

--- BaseDebian.sh
+++ BaseDebian.sh
@@ -4,6 +4,7 @@
 ########################################################################
 #Run as user:                                                          #
 #PATH=/home/$USER/github                                               #
+#sudo apt modernize-sources
 #sudo apt update && sudo apt upgrade                                   #
 #sudo apt install git -y                                               #
 #mkdir $PATH                                                           #
