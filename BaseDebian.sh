e4b93d99688f4cfba21ba9619542362d3b5b60da
Jackblow33 <jackblow33@hotmail.com>
2025-03-29 16:46:57 -0400

    Update BaseDebian.sh

--- BaseDebian.sh
+++ BaseDebian.sh
@@ -35,7 +35,8 @@ PKGS=(
         'fastfetch'                 # Fetching system information in terminal
         'numlockx'                  # Turn NumLock on
         'gparted'                   # Partition utility
-        'ark unrar rar'             #ark manages various archive formats
+        'ark unrar rar'             # ark manages various archive formats
+        'vlc'                       # VLC is free software to play, transcode and broadcast video and audio files
 #        'kde-config-sddm'           # Login Screen (SDDM) System Settings Module
 #        'software-properties-qt'    # allows you to easily manage your distribution and independent software vendor software sources
 )
