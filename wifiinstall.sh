adb8d8e5905e80dad2af9c0a827684614ff3e51c
Jackblow33 <jackblow33@hotmail.com>
2025-03-29 18:34:08 -0400

    Update wifiinstall.sh

--- wifiinstall.sh
+++ wifiinstall.sh
@@ -4,7 +4,9 @@
 #wifiinstall       - imac BCM4360 & +++
 #From Google search = broadcom-wl-dkms   debian trixie how to install
 
-sudo apt install linux-image-amd64 linux-headers-amd64 broadcom-sta-dkms wireless-tools
+cd /home/jack/Downloads
+sudo wget http://http.us.debian.org/debian/pool/non-free/b/broadcom-sta/broadcom-sta-dkms_6.30.223.271-26_amd64.deb
+sudo apt install linux-image-amd64 linux-headers-amd64 wireless-tools #broadcom-sta-dkms
 
 #
 Unloading and reloading modules
