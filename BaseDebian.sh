3c3aaeb0b10a1d8f6b9597451ce9e1a35aa7b973
Jackblow33 <jackblow33@hotmail.com>
2025-03-29 02:30:24 -0400

    Update BaseDebian.sh

--- BaseDebian.sh
+++ BaseDebian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ########################################################################
 # ENTER THE FOLLOWING COMMANDS BEFORE LOADING THIS SCRIPT ##############
-#As user:
+#Run as user:
 #PATH=/home/$USER/github                                               #
 #sudo apt update && sudo apt upgrade                                   #
 #sudo apt install git -y                                               #
