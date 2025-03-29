336594a9833f859ebec0d9267ad679f63fa0172d
Jackblow33 <jackblow33@hotmail.com>
2025-03-29 16:42:01 -0400

    Add files via upload

---
+++ wifiinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#wifiinstall       - imac BCM4360 & +++
+#From Google search = broadcom-wl-dkms   debian trixie how to install
+
+sudo apt install linux-image-amd64 linux-headers-amd64 broadcom-sta-dkms wireless-tools
+
+#
+Unloading and reloading modules
+modprobe -r wl && sudo modprobe wl
