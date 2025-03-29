c04e6dbed01e7d99e8bcbb25f33fb65d2c271362
Jackblow33 <jackblow33@hotmail.com>
2025-03-29 19:46:19 -0400

    Update host2vm.sh

--- host2vm.sh
+++ host2vm.sh
@@ -10,15 +10,15 @@
 #https://medium.com/@max.kombarov/install-the-spice-guest-agent-tools-on-a-debian-based-system-in-vm-2bc66fc2d95b
 ####### install on x11 virtualize system only...???
 
-apt update
-apt install spice-vdagent
+sudo apt update
+sudo apt install spice-vdagent
 ##Start the SPICE Agent Service
 systemctl start spice-vdagent
 ##Start automatically at boot
 ###???sudo systemctl enable spice-vdagent
 ##Check the SPICE Agent Status
-#systemctl status spice-vdagentsudo
+systemctl status spice-vdagentsudo
 #OLD_sudo nano /usr/lib/systemd/user/plasma-core.target
 #Add: spice-vdagent.service at the end of line: Wants=
-
+read -p "ok"
 
