00e078ca8056ebee2c457bf1fd7ed3a784aba7a0
Jackblow33 <jackblow33@hotmail.com>
2025-04-05 00:13:35 -0400

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#Test first: https://extensions.gnome.org/extension/307/dash-to-dock/
 #https://wiki.debian.org/Gnome
+
 
 sudo apt update && sudo apt upgrade
 sudo apt install gnome-core  #or gnome-base    https://www.youtube.com/watch?v=zy-5UHC3t-Q
@@ -12,7 +12,21 @@ read -p "Press Enter to edit"
 sudo nano /etc/NetworkManager/NetworkManager.conf
 #Check system|network|IPv4 and make sure static ip address or dhcp is set properly
 sudo apt-install gnome-session nautilus gnome-terminal git firefox-esr wget gedit
+#echo "Press [enter] to reboot"; read enterKey
+#sudo shutdown -r now #reboot
+
+##Install gnome browser evtensions first: https://extensions.gnome.org/ OR better next
+sudo apt install gnome-shell-extension-prefs
+#Add dash to dock: https://extensions.gnome.org/extension/307/dash-to-dock/
+cd /home/$USER/Download
+wget https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip
+unzip dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip \ -d ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/
+#Add minimize / maximize buttons
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+#Log out & login. That's it
+echo "Press [enter] to reboot"; read enterKey
 sudo shutdown -r now #reboot
+
 
 
 
