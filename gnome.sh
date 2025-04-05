a3f11aa398cda107798e049e5b6512ba9c3b631d
Jackblow33 <jackblow33@hotmail.com>
2025-04-05 00:53:11 -0400

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -12,23 +12,23 @@ read -p "Press Enter to edit"
 sudo nano /etc/NetworkManager/NetworkManager.conf
 #Check system|network|IPv4 and make sure static ip address or dhcp is set properly
 sudo apt-install gnome-session nautilus gnome-terminal git firefox-esr wget gedit
-#echo "Press [enter] to reboot"; read enterKey
-#sudo shutdown -r now #reboot
-
-##Install gnome browser evtensions first: https://extensions.gnome.org/ OR better next
-sudo apt install gnome-shell-extension-prefs
-#Add dash to dock: https://extensions.gnome.org/extension/307/dash-to-dock/
-cd /home/$USER/Download
-wget https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip
-unzip dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip \ -d ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/
-#Add minimize / maximize buttons
-gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed true
-gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
-#Log out & login. That's it
 echo "Press [enter] to reboot"; read enterKey
 sudo shutdown -r now #reboot
-#READ https://micheleg.github.io/dash-to-dock/download.html
+
+###I#nstall gnome browser evtensions first: https://extensions.gnome.org/ OR better next
+#sudo apt install gnome-shell-extension-manager  #gnome-shell-extension-prefs
+###Add dash to dock: https://extensions.gnome.org/extension/307/dash-to-dock/
+#cd /home/$USER/Download
+#wget https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip
+#unzip dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip \ -d ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/
+###Add minimize / maximize buttons
+#gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+#gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed true
+#gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
+###Log out & login. That's it
+#echo "Press [enter] to reboot"; read enterKey
+#sudo shutdown -r now #reboot
+###READ https://micheleg.github.io/dash-to-dock/download.html
 
 
 
