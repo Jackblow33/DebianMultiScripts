4afb8e3df0c21978d118657f6c81ab2d66fd5a9f
Jackblow33 <jackblow33@hotmail.com>
2025-04-05 18:53:25 -0400

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -3,6 +3,8 @@
 #https://wiki.debian.org/Gnome  &&  https://www.youtube.com/watch?v=zy-5UHC3t-Q
 
 
+
+#VARIABLE
 PKGS=(
         'gnome-core'                #  Core components of the GNOME Desktop environment
         'gedit'                     # Text editor
@@ -18,6 +20,24 @@ PKGS=(
         'vlc'                       # VLC is free software to play, transcode and broadcast video and audio files
 )
 
+#fonctions
+timer_start()
+{
+BEGIN=$(date +%s)
+}
+
+#fonctions
+timer_stop()
+{
+    NOW=$(date +%s)
+    let DIFF=$(($NOW - $BEGIN))
+    let MINS=$(($DIFF / 60))
+    let SECS=$(($DIFF % 60))
+    echo Time elapsed: $MINS:`printf %02d $SECS`
+}
+
+
+timer_start
 sudo apt update && sudo apt upgrade
 
 #Packages installer
@@ -29,13 +49,9 @@ done
 #remove apps
 sudo apt purge ifupdown gnome-text-editor -y && sudo apt autoremove gnome-text-editor -y
 
-sudo sed -i "s/managed=false/managed=true/" /etc/NetworkManager/NetworkManager.conf
-echo "#########################################################"
-echo "# Editing NetworkManager: managed=false to managed=true #"
-echo "#########################################################"
-read -p "Press Enter to edit"
-sudo nano /etc/NetworkManager/NetworkManager.conf
+sudo sed -i "s/managed=false/managed=true/" /etc/NetworkManager/NetworkManager.conf  #Editing NetworkManager file: managed=false to managed=true
 #Check system|network|IPv4 and make sure static ip address or dhcp is set properly
 
+timer_stop
 echo "Press [enter] to reboot"; read enterKey
 sudo shutdown -r now      #reboot
