15f306f4b3d9b17d38e064dd00f78f79db7575f5
Jackblow33 <jackblow33@hotmail.com>
2025-04-06 15:31:47 -0400

    Update gnome-extensions-final.sh

--- gnome-extensions-final.sh
+++ gnome-extensions-final.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 
-#Enabling dash-to-dock & adding minimize/maximize/close buttons at first boot
+#Enabling dash-to-dock, freon & adding minimize/maximize/close buttons at first boot
 gnome-extensions enable dash-to-dock@micxgx.gmail.com
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+gnome-extensions enable freon@UshakovVasilii_Github.yahoo.com
 
 
 systemctl disable gnome-post-install.service 
