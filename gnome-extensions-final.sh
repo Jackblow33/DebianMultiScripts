988cf6c9331a11856e01ccaeed2877ccce27050f
Jackblow33 <jackblow33@hotmail.com>
2025-04-07 18:19:37 -0400

    Update gnome-extensions-final.sh

--- gnome-extensions-final.sh
+++ gnome-extensions-final.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+#Source https://askubuntu.com/questions/156771/run-a-script-only-at-the-very-first-boot
+
 #Enabling dash-to-dock, freon & adding minimize/maximize/close buttons at first boot
 gnome-extensions enable dash-to-dock@micxgx.gmail.com
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
